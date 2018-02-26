62d05de87c86ebf5d98fb849823b247fcc4a5d53
Victor Sales <salesrvictor@gmail.com>
2018-02-25 21:04:59 -0300

    put ScreenBase.py in models

---
+++ models/ScreenBase.py
@@ -0,0 +1,20 @@
+class ScreenBase:
+
+    def __init__(self):
+        self.next = self
+
+    def process_input(self, events):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+    def switch_to_scene(self, next_scene):
+        self.next = next_scene
+
+    def terminate(self):
+        self.switch_to_scene(None)
+
