058dca9ce5335b82a10c8a927b199d7b465681af
Victor Sales <salesrvictor@gmail.com>
2018-02-26 17:21:54 -0300

    added ScreenBase and TitleScreen to ui

---
+++ ui/TitleScreen.py
@@ -0,0 +1,8 @@
+class TitleScreen(ScreenBase):
+
+    def __init__(self):
+        super().__init__()
+
+    def render(self, screen):
+        # TODO: write actual redering
+        screen.fill(0,255,0)
