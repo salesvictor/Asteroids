0b2f66648405d779d80d8e511a657a3062d175b7
Diego Teixeira Barreto Lima <diegoblima2010@gmail.com>
2018-03-01 10:27:41 -0300

    makes Ship class usefull

--- models/Ship.py
+++ models/Ship.py
@@ -1,10 +1,29 @@
-from models.GameObject import GameObject
+import os
+import sys
+import pygame
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from models.GameObject import GameObject
 
 class Ship(GameObject):
 
     def __init__(self, x, y, screen):
         super().__init__(x, y, screen)
+
+        # Initializing ship image in graphics folder
+        path = os.path.dirname(__file__)
+        path = os.path.join(path, os.pardir, 'graphics', 'ship.png')
+        self.image = pygame.image.load(path)
+
+        # Resizing image
+        img_size = self.image.get_rect().size
+        new_img_size = []
+        for dimension in img_size:
+            new_img_size.append(int(0.1*dimension))
+        self.image = pygame.transform.scale(self.image, new_img_size)
+
+        # Use x and y as the dimensions of the image.
+        self.rect = (x, y)
 
     def render(self):
         # TODO(Victor) write render
