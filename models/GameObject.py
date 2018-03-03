44916f62b298aaf7c1a4bafda5a359b3c429ead2
Victor Sales <salesrvictor@gmail.com>
2018-03-03 14:10:49 -0300

    added test scenario, changed GameObject constructor and Ship is now ready except for firing

--- models/GameObject.py
+++ models/GameObject.py
@@ -1,21 +1,45 @@
+import os
 import pygame
+
 
 class GameObject(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, screen):
+    def __init__(self, x, y, direction, vel_dir, speed, screen, img_name, img_factor):
         super().__init__()
         self.x = x
         self.y = y
+        self.speed = speed
         self.screen = screen
+        self.vel_dir = vel_dir
+        self.direction = direction
 
-    def on_border(self):
-        if self.x < 0 or self.x > self.screen.width:
-            self.x = (self.x + self.screen.width) % self.screen.width
+        # Initializing ship image in graphics folder
+        path = os.path.dirname(__file__)
+        path = os.path.join(path, os.pardir, 'graphics', img_name)
+        self.original_image = pygame.image.load(path)
 
-        if self.y < 0 or self.y > self.screen.height:
-            self.y = (self.y + self.screen.width) % self.screen.width
+        # Resizing image
+        img_size = self.original_image.get_rect().size
+        new_img_size = []
+        for dimension in img_size:
+            new_img_size.append(int(img_factor * dimension))
+        self.original_image = pygame.transform.scale(self.original_image, new_img_size)
+        self.image = self.original_image
 
-    def render(self):
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def check_on_border(self):
+        width, height = self.screen.get_size()
+
+        print(self.x, self.y, self.direction, self.speed)
+
+        if self.x < 0 or self.x > width:
+            self.x = (self.x + width) % width
+
+        if self.y < 0 or self.y > height:
+            self.y = (self.y + height) % height
+
+    def update(self):
         pass
 
     def destroy(self):
--- models/Ship.py
+++ models/Ship.py
@@ -1,33 +1,39 @@
 import os
 import sys
 import pygame
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from models.GameObject import GameObject
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
 class Ship(GameObject):
+    ACCEL = 0.5
+    DEACCEL = 0.1
+    MAX_SPEED = 5
 
-    def __init__(self, x, y, screen):
-        super().__init__(x, y, screen)
+    def __init__(self, screen, x, y):
+        # super().__init__(screen.width//2, screen.height//2, (0, 0), screen)
+        super().__init__(x, y, 0, (0, 0), 0, screen, 'ship.png', 0.1)
+        self.speed = 0
 
-        # Initializing ship image in graphics folder
-        path = os.path.dirname(__file__)
-        path = os.path.join(path, os.pardir, 'graphics', 'ship.png')
-        self.image = pygame.image.load(path)
+    def update(self):
+        self.x += self.speed * self.vel_dir[0]
+        self.y -= self.speed * self.vel_dir[1]
+        self.rect = self.image.get_rect(center=(self.x, self.y))
 
-        # Resizing image
-        img_size = self.image.get_rect().size
-        new_img_size = []
-        for dimension in img_size:
-            new_img_size.append(int(0.1*dimension))
-        self.image = pygame.transform.scale(self.image, new_img_size)
+        self.check_on_border()
 
-        # Use x and y as the dimensions of the image.
-        self.rect = (x, y)
+    def turn(self, angle):
+        self.image = pygame.transform.rotate(self.original_image, self.direction + angle)
+        self.direction += angle
+        self.rect = self.image.get_rect(center=(self.x, self.y))
 
-    def render(self):
-        # TODO(Victor) write render
-        pass
+    def accelerate(self, vel_dir):
+        self.speed = min(self.speed+self.ACCEL, self.MAX_SPEED)
+        self.vel_dir = vel_dir
+
+    def stop(self):
+        self. speed = max(self.speed-self.DEACCEL, 0)
 
     def destroy(self):
         # TODO(Victor) write destroy
