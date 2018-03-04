31ef79f01fc1dcd383985f6d01dff956e19c7831
Victor Sales <salesrvictor@gmail.com>
2018-03-04 16:45:01 -0300

    added SPACE functionality to change background color in TestShipMovement

--- tests/TestShipMovement.py
+++ tests/TestShipMovement.py
@@ -1,35 +1,51 @@
-import pygame
-from math import cos, sin, radians
+import pygame as pg
 from models.Ship import Ship
+
+
+def change_color(screen):
+    color = screen.get_at((0, 0))
+
+    if color == (0, 0, 0):
+        return (0, 0, 255)
+    else:
+        return (0, 0, 0)
+
 
 if __name__ == '__main__':
     FPS = 60
     SCREEN_W = 800
     SCREEN_H = 600
     SIZE = (SCREEN_W, SCREEN_H)
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+    WHITE = (255, 255, 255)
 
-    pygame.init()
-    pygame.font.init()
+    bg_color = BLACK
 
-    screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption('Ship Movement Testing')
-    my_font = pygame.font.SysFont('Times New Roman', 15)
-    clock = pygame.time.Clock()
+    pg.init()
+    pg.font.init()
+
+    screen = pg.display.set_mode(SIZE)
+    pg.display.set_caption('Ship Movement Testing')
+    my_font = pg.font.SysFont('Times New Roman', 15)
+    clock = pg.time.Clock()
 
     # Creates a sprite list for drawing
-    sprites = pygame.sprite.Group()
+    sprites = pg.sprite.Group()
 
     # Creates a new ship and adds it to the sprite list
     player = Ship(screen, SCREEN_W//2, SCREEN_H//2)
     sprites.add(player)
 
     while True:
-        ev = pygame.event.poll()
-        if ev.type == pygame.QUIT:
+        ev = pg.event.poll()
+        if ev.type == pg.QUIT:
             break
-        elif ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
+        elif ev.type == pg.KEYDOWN:
+            if ev.key == pg.K_ESCAPE:
                 break
+            if ev.key == pg.K_SPACE:
+                bg_color = change_color(screen)
 
         debug_text = (f'(pos, center, direction, speed, vel_dir) = '
                       f'(({player.x:3.0f}, {player.y:3.0f}), {player.rect.center}, '
@@ -38,12 +54,12 @@ if __name__ == '__main__':
         text_surface = my_font.render(debug_text, True, (255, 255, 255))
         (font_w, font_h) = text_surface.get_size()
 
-        screen.fill((0, 0, 0))
+        screen.fill(bg_color)
 
         sprites.update()
         sprites.draw(screen)
         screen.blit(text_surface, ((SCREEN_W-font_w)/2, 0))
-        pygame.display.flip()
+        pg.display.flip()
         clock.tick(FPS)
 
-    pygame.quit()
+    pg.quit()
