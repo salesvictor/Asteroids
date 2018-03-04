b32c391ce7cf6682ff04a62be205377e8eba76ab
Victor Sales <salesrvictor@gmail.com>
2018-03-04 17:03:27 -0300

    added change_color to TestAsteroidsMovement

--- tests/TestAsteroidsMovement.py
+++ tests/TestAsteroidsMovement.py
@@ -1,20 +1,35 @@
-import pygame
+import pygame as pg
 from models.SmallAsteroid import SmallAsteroid
 from models.MediumAsteroid import MediumAsteroid
 from models.BigAsteroid import BigAsteroid
+
+def change_color(screen):
+    color = screen.get_at((0, 0))
+
+    if color == (0, 0, 0):
+        return (0, 0, 255)
+    else:
+        return (0, 0, 0)
+
 
 if __name__ == '__main__':
     FPS = 60
     SCREEN_W = 800
     SCREEN_H = 600
     SIZE = (SCREEN_W, SCREEN_H)
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+    WHITE = (255, 255, 255)
 
-    pygame.init()
-    screen = pygame.display.set_mode(SIZE)
-    clock = pygame.time.Clock()
-    pygame.display.set_caption('Asteroid Testing')
+    bg_color = BLACK
 
-    asteroids = pygame.sprite.Group()
+    pg.init()
+
+    screen = pg.display.set_mode(SIZE)
+    pg.display.set_caption('Asteroid Movement Testing')
+    clock = pg.time.Clock()
+
+    asteroids = pg.sprite.Group()
     for i in range(12):
         if i % 3 == 0:
             asteroids.add(SmallAsteroid(screen))
@@ -24,19 +39,21 @@ if __name__ == '__main__':
             asteroids.add(BigAsteroid(screen))
 
     while True:
-        ev = pygame.event.poll()
-        if ev.type == pygame.QUIT:
+        ev = pg.event.poll()
+        if ev.type == pg.QUIT:
             break
-        elif ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
+        elif ev.type == pg.KEYDOWN:
+            if ev.key == pg.K_ESCAPE:
                 break
+            if ev.key == pg.K_SPACE:
+                bg_color = change_color(screen)
 
-        screen.fill((0, 0, 0))
+        screen.fill(bg_color)
 
         asteroids.update()
         asteroids.draw(screen)
 
-        pygame.display.flip()
+        pg.display.flip()
         clock.tick(FPS)
 
-    pygame.quit()
+    pg.quit()
