7a9be8a061074b4befbe892e9b13ec6fe82b87f3
Victor Sales <salesrvictor@gmail.com>
2018-03-04 13:41:31 -0300

    recycled code in Bullet

--- models/Bullet.py
+++ models/Bullet.py
@@ -3,23 +3,19 @@ from models.GameObject import GameObject
 
 
 class Bullet(GameObject):
+    LIFE_TIME = 40
+
     def __init__(self, screen,  x, y, direction):
-        super().__init__(x, y, direction, (cos(radians(direction)), sin(radians(direction))), 10, screen, 'bullet.png', 0.5)
-        self.life_time = 40
+        super().__init__(x, y, direction, (cos(radians(direction)), sin(radians(direction))),
+                         10, screen, 'bullet.png', 0.5)
         self.age = 0
 
     def update(self):
+        super().update()
+
         self.age += 1
-
-        if self.age > self.life_time:
+        if self.age > self.LIFE_TIME:
             self.destroy()
-
-        else:
-            self.x += self.speed * self.vel_dir[0]
-            self.y += self.speed * self.vel_dir[1]
-            self.rect = self.image.get_rect(center=(self.x, self.y))
-
-        self.check_on_border()
 
     def destroy(self):
         self.kill()
