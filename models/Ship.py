2955c29aea3fe3bafe2bb238aacee68d77c07f83
Victor Sales <salesrvictor@gmail.com>
2018-03-05 21:33:58 -0300

    added bullets to Ship

--- models/Ship.py
+++ models/Ship.py
@@ -9,15 +9,17 @@ class Ship(GameObject):
     DEACCEL = 0.09
     MAX_SPEED = 4.5
     SHOT_DELAY = 5
-    TURN_SPEED = 5
+    TURN_SPEED = -5
 
     def __init__(self, screen, x, y):
         super().__init__(x, y, 0, (0, 0), 0, screen, 'ship.png', 0.1)
         self.shot_bullets = []
+        self.bullets_sprite = pg.sprite.Group()
 
     def update(self):
         self.speed = max(self.speed - self.DEACCEL, 0)
 
+        self.bullets_sprite.update()
         super().update()
 
     def turn(self, angle):
@@ -29,6 +31,7 @@ class Ship(GameObject):
         if not self.shot_bullets or self.shot_bullets[-1].age > self.SHOT_DELAY:
             bullet = Bullet(self.screen, self.x, self.y, -self.direction)
             self.shot_bullets.append(bullet)
+            self.bullets_sprite.add(bullet)
 
     def destroy(self):
         # TODO(Victor) write destroy
