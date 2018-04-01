c8f1c2060671444e20325d622eb589c8a5c9f244
juanfdg <juanfdantas@gmail.com>
2018-04-01 13:42:19 -0300

    Changed bullet mask
    Changed bullet mask to consider the bullet trajectory, avoiding tunelling in collisions

--- models/Bullet.py
+++ models/Bullet.py
@@ -1,3 +1,4 @@
+import pygame as pg
 from math import sin, cos, radians
 from models.GameObject import GameObject
 
@@ -8,7 +9,7 @@ class Bullet(GameObject):
 
     def __init__(self, screen,  x, y, direction):
         super().__init__(x, y, direction, [self.SPEED_MODULE*cos(radians(direction)),
-                         self.SPEED_MODULE*sin(radians(direction))], screen, 'bullet.png', 0.9)
+                         self.SPEED_MODULE*sin(radians(direction))], screen, 'bullet.png', 0.6)
 
         self.age = 0  # Number of cycles since the bullet was cast
 
@@ -18,6 +19,12 @@ class Bullet(GameObject):
             self.kill()
 
         super().update()
+
+        last_pos = [self.mask_surface.get_rect().center[0] - int(self.speed[0]),
+                    self.mask_surface.get_rect().center[1] - int(self.speed[1])]
+        # Drawing the bullets trajectory to mask surface to prevent from tunneling
+        pg.draw.line(self.mask_surface, (200, 150, 150), last_pos, self.mask_surface.get_rect().center, 5)
+        self.mask = pg.mask.from_surface(self.mask_surface)
 
     def kill(self):
         super().kill()
